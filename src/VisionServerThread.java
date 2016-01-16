15a8a5bb09b47e8918691655206c77af0d60a105
Ben Nichols-Farquhar <erogone775@gmail.com>
2016-01-15 22:20:42 -0500

    small changes to serverthread changed to have different responce and change modes

--- src/VisionServerThread.java
+++ src/VisionServerThread.java
@@ -6,31 +6,42 @@ public class VisionServerThread extends Thread
 {
     protected DatagramSocket socket = null;
     private boolean running = true;
-    
+    public int port = 00000;
+    public InetAddress address = null;
+            
     public VisionServerThread() throws IOException{
         this("VisionSeverThread");
     }
     public VisionServerThread(String name) throws IOException{
         super(name);
-        socket = new DatagramSocket(1234);
+        socket = new DatagramSocket(31415);
     }
     public void run(){
         if (socket == null){
             return;
         }
         else {
+            byte[] buf = new byte[1024];
             while(running){
                 try{
-                    byte[] buf = new byte[1024];
+                    
                     
                     DatagramPacket packet = new DatagramPacket(buf,buf.length);
                     socket.receive(packet);
-                    InetAddress address = packet.getAddress();
-                    int port = packet.getPort();
                     
-                    String response = "Hello world";
+                    address = packet.getAddress();
+                    port = packet.getPort();
+                    byte[] receivedPacket =  new byte[1024];
+                    receivedPacket = packet.getData();
                     
-                    buf = response.getBytes();
+                    if (Arrays.toString(receivedPacket) == "start"){
+                        String response = "Hello world";
+                        buf = response.getBytes();
+                    }
+                    else {
+                        String response = "Hello world";
+                        buf = response.getBytes();
+                    }
                     
                     DatagramPacket responsePacket = new DatagramPacket(buf,buf.length,address,port);
                     socket.send(responsePacket);
