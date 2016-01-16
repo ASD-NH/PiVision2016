1eacae814908d35b2708cab5d72858a2a2a23be9
Kurtis <kurtis@Kurtis-Laptop>
2016-01-16 13:08:41 -0500

    Added rudimentary test code to display the video feed from a webcam. (Never use this commit, Stephan is going to fix it right now)

--- src/VisionProcessingThread.java
+++ src/VisionProcessingThread.java
@@ -1,10 +1,17 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics.*;
+import java.awt.GraphicsEnvironment;
 import java.awt.color.ColorSpace;
 import java.awt.image.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
 import magick.*;
 import magick.util.*;
 
@@ -23,9 +30,18 @@ public class VisionProcessingThread extends Thread{
     }
     
     public void run() {
+    	MagickCanvas display = new MagickCanvas();
+    	display.setEnabled(true);
+    	display.setVisible(true);
+    	JFrame f = new JFrame("Debug Display");
+    	f.setSize(300, 400);
+    	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	f.setVisible(true);
     	while(m_running) {
     		//m_image is the webcam stream
     		m_image = m_webcam.getImage();
+    		display.setImage(m_image);
+    		display.update(f.getGraphics());
     	}
     }
 
