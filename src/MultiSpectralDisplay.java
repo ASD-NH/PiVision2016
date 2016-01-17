c41b18fd26a2baff41b2f6067d05497448270a37
Stephan Lensky <stephan.lensky@gmail.com>
2016-01-17 14:54:33 -0500

    Added Debug Display
    Created new class to deal with displaying everything, and added the
    update function to the main loop in the processing thread.

---
+++ src/MultiSpectralDisplay.java
@@ -0,0 +1,36 @@
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+
+import boofcv.gui.image.ImagePanel;
+import boofcv.gui.image.ShowImages;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.image.ImageUInt8;
+import boofcv.struct.image.MultiSpectral;
+
+public class MultiSpectralDisplay {
+    
+    private ImagePanel m_display;
+    Dimension m_resolution;
+    private BufferedImage m_image = null;
+    
+    MultiSpectralDisplay(Dimension res) {
+        
+        m_display = new ImagePanel();
+        m_resolution = res;
+        //init display window
+        m_display.setPreferredSize(m_resolution);
+        ShowImages.showWindow(m_display, "Debug Display", true);
+        
+    }
+    
+    //set source to the RGB image (to be displayed)
+    void setImageRGB(MultiSpectral<ImageUInt8> source) {
+        
+        //convert to displayable format
+        m_image = ConvertBufferedImage.convertTo_U8(source, m_image, true);
+        //set image
+        m_display.setBufferedImageSafe(m_image);
+        
+    }
+    
+}
--- src/VisionProcessingThread.java
+++ src/VisionProcessingThread.java
@@ -1,11 +1,5 @@
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics.*;
-import java.awt.GraphicsEnvironment;
-import java.awt.color.ColorSpace;
-import java.awt.image.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -14,27 +8,46 @@ import javax.swing.JFrame;
 import boofcv.struct.*;
 import boofcv.struct.image.ImageUInt8;
 import boofcv.struct.image.MultiSpectral;
+import boofcv.gui.image.*;
+import boofcv.io.image.ConvertBufferedImage;
 
 public class VisionProcessingThread extends Thread{
     
-    private Dimension m_camRes = new Dimension(640, 480);
+    //Camera quality values
+    static final Dimension MAX = new Dimension(640, 480);
+    static final Dimension MED = new Dimension(320, 240);
+    static final Dimension MIN = new Dimension(176, 144);
+    
+    //set camera resolution
+    private Dimension m_camRes = MAX;
+    
 	private CameraInterface m_webcam;
 	private MultiSpectral<ImageUInt8> m_image;
 	private boolean m_running = true;
+	
+	private MultiSpectralDisplay m_display;
 	
     public VisionProcessingThread(){
         this("VisionProcessingThread");
     }
     public VisionProcessingThread(String name){
         super(name);
-        m_webcam = new CameraInterface(1, m_camRes);
+        //init webcam
+        m_webcam = new CameraInterface(m_camRes);
+        //init display
+        m_display = new MultiSpectralDisplay(m_camRes);
     }
     
     public void run() {
     	while(m_running) {
-    		//m_image is the webcam stream
+    		//m_image is the current webcam image
     		m_image = m_webcam.getImage();
+    		
+    		
+    		
+    		//debug display update image
+    		m_display.setImageRGB(m_image);
     	}
     }
-
+    
 }
