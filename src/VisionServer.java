ec7163844e9b9373fb80cbd6ff8b1d3fc1921dd1
Stephan Lensky <stephan.lensky@gmail.com>
2016-01-18 20:40:04 -0500

    Raspberry Pi Compatibility
    Previous commit failed, recommiting. See previous commit message for
    details on what this should have been.

--- src/VisionServer.java
+++ src/VisionServer.java
@@ -3,9 +3,16 @@ import java.lang.*;
 import java.io.*;
 
 public class VisionServer{
+    
+    //Uncomment this out when deploying to the pi (sets the webcam driver to the pi one)
+    static {
+        //Webcam.setDriver(new V4l4jDriver());
+    }
+    
     public static void main(String[] args) throws IOException
-        {
-            //new VisionServerThread().start();
-            new VisionProcessingThread().start();
-        }
+    {
+        new VisionServerThread().start();
+        new VisionProcessingThread().start();
+    }
+    
 }
