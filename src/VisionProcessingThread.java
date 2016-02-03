c96cf9ea8c7006d8f6b292206f23ec76907d4988
Kurtis <kurtis@Kurtis-Laptop>
2016-02-02 20:13:22 -0500

    Fixed Ball detection code. Very strict filters that work extremely well in good lighting conditions but needs to be refitted to work with the red LED's on the robot.

--- src/VisionProcessingThread.java
+++ src/VisionProcessingThread.java
@@ -3,21 +3,30 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.List;
+
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+
 import boofcv.struct.*;
 import boofcv.struct.image.ImageFloat32;
 import boofcv.struct.image.ImageSInt16;
 import boofcv.struct.image.ImageUInt8;
 import boofcv.struct.image.MultiSpectral;
+import georegression.struct.shapes.EllipseRotated_F64;
 import boofcv.alg.color.ColorHsv;
 import boofcv.alg.feature.detect.edge.CannyEdge;
+import boofcv.alg.feature.detect.edge.EdgeContour;
 import boofcv.alg.filter.binary.BinaryImageOps;
+import boofcv.alg.filter.binary.Contour;
 import boofcv.alg.filter.binary.GThresholdImageOps;
 import boofcv.alg.filter.binary.ThresholdImageOps;
 import boofcv.alg.misc.PixelMath;
+import boofcv.alg.shapes.FitData;
+import boofcv.alg.shapes.ShapeFittingOps;
 import boofcv.core.image.ConvertImage;
 import boofcv.factory.feature.detect.edge.FactoryEdgeDetectors;
+import boofcv.gui.feature.VisualizeShapes;
 import boofcv.gui.image.*;
 import boofcv.io.image.ConvertBufferedImage;
 import java.net.*;
@@ -136,6 +145,27 @@ public class VisionProcessingThread extends Thread{
         return towerData;
     }
     //code to find the ball
+    
+    private EllipseRotated_F64 largestArea(List<EllipseRotated_F64> ellipses){
+    	EllipseRotated_F64 largest = null;
+    	
+    	for(EllipseRotated_F64 e : ellipses){
+    		if(largest == null){
+    			largest = e;
+    		}
+    		else {
+    			double largeArea = Math.PI * largest.a * largest.b;
+    			double newArea = Math.PI * e.a * e.b;
+    			
+    			if(newArea > largeArea){
+    				largest = e;
+    			}
+    		}
+    	}
+    	
+    	return largest;
+    }
+    
     private int[] findBall() {
         int[] ballData = new int[9];
         
@@ -149,6 +179,7 @@ public class VisionProcessingThread extends Thread{
         
         //extracts just the value band from the hsv image
         ConvertImage.convert(hsvImage.getBand(2), valueBand);
+        //valueBand = m_image.getBand(0).clone();
         
         //threshold the image to make the ball clear
         valueBand = ThresholdImageOps.localSquare(valueBand, null, 20, 0.98f, true, null, null);
@@ -156,12 +187,39 @@ public class VisionProcessingThread extends Thread{
         //edge detect to locate the ball
         CannyEdge<ImageUInt8, ImageSInt16> canny = FactoryEdgeDetectors.canny(2,true, true, ImageUInt8.class, ImageSInt16.class);
         canny.process(valueBand, 0.1f, 0.3f, valueBand);
+        BufferedImage gImage = new BufferedImage(valueBand.width, valueBand.height, 4);
+        Graphics2D g = gImage.createGraphics();
+        g.setStroke(new BasicStroke(2));
         
-        //update m_image to be used for displaying the result
-        PixelMath.multiply(valueBand, 255, valueBand);
-        m_image.setBand(0, valueBand);
-        m_image.setBand(1, valueBand);
-        m_image.setBand(2, valueBand);
+        ConvertBufferedImage.convertTo_U8(m_image, gImage, true);
+        
+        List<Contour> contours = BinaryImageOps.contour(valueBand, ConnectRule.EIGHT, null);
+        
+        List<EllipseRotated_F64> validEllipses = new ArrayList<EllipseRotated_F64>();
+        
+        for(Contour c : contours){
+        	FitData<EllipseRotated_F64> circle = ShapeFittingOps.fitEllipse_I32(c.external, 0, false, null);
+        	
+        	double ratio = circle.shape.a / circle.shape.b;
+        	if(ratio < 1){
+        		ratio = 1 / ratio;
+        	}
+        	
+        	double averageRadius = (circle.shape.a + circle.shape.b) / 2;
+        	
+        	if(ratio < 1.08 && circle.error < 0.1 && averageRadius > 20){
+        		validEllipses.add(circle.shape);
+        	}
+        }
+        
+        EllipseRotated_F64 ball = largestArea(validEllipses);
+        if(ball != null){
+	        g.setColor(Color.CYAN);
+	        double averageRadius = (ball.a + ball.b) / 2;
+	        g.drawOval((int)(ball.center.x - averageRadius), (int)(ball.center.y - averageRadius), (int)averageRadius * 2, (int)averageRadius * 2);
+        }
+        
+        m_image = ImageConversion.toMultiSpectral(gImage);
         
         return ballData;
     }
